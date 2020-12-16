6f668d5bb497674b89a9928c501f17ad383863bb
GAMERFUCKER <bsdkbhaag248@gmail.com>
2020-12-16 18:36:45 +0530

    Update group.py

--- telebot/plugins/group.py
+++ telebot/plugins/group.py
@@ -8,7 +8,7 @@ async def join(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         await eor(
             e,
-            "This is my community.\n\n[Channel](http://t.me/giveaways_24hrs)\n\n[Chat Group](https://t.me/giveaways24hrsdiscuss)\n\n[UserBot Tutorial - TeleBot](https://t.me/TeleBotHelp)\n\n[TeleBot Chat](https://t.me/TeleBotHelpChat)\n\n[Github](https://github.com/xditya)\n\n[YouTube](https://bit.ly/adityas7)",
+            "This is my community.\n\n[Channel](http://t.me/Gamerzbots)\n\n[Chat Group](https://t.me/miakhalifachatgroup)\n\n[UserBot Tutorial - GamerzBot](https://t.me/Gamerzbots)\n\n[GamerzBot Chat](https://t.me/miakhalifachatgroup)\n\n[Github](https://github.com/gamerfuckerofficial)\n\n[YouTube](https://youtube.com/channel/UCOAKFN1uKUhSYcnrQhytBng)",
         )
 
 
