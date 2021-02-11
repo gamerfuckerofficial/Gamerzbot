9dd2099d6420eef51eba19a4ca48999fd49479dc
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-02-11 22:02:01 +0530

    Update _help.py

--- telebot/plugins/_help.py
+++ telebot/plugins/_help.py
@@ -21,7 +21,7 @@ from telebot.telebotConfig import Config
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "GamerzBot User"
 CMD_HNDLR = Config.CMD_HNDLR
-CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "🌟")
+CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "💢")
 
 if CMD_HNDLR is None:
     CMD_HNDLR = "."
