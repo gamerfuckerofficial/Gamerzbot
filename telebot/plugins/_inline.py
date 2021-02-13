ea9d967e516f272ffcfe68d05c8198552c4d1f9c
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-02-13 21:31:35 +0530

    Update _inline.py

--- telebot/plugins/_inline.py
+++ telebot/plugins/_inline.py
@@ -68,7 +68,7 @@ elif Var.LOAD_MYBOT == "False":
         )
     )
 
-CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "⚡")
+CUSTOM_HELP_EMOJI = os.environ.get("CUSTOM_HELP_EMOJI", "🌟")
 HELP_ROWS = int(os.environ.get("HELP_ROWS", 5))
 HELP_COLOUMNS = int(os.environ.get("HELP_COLOUMNS", 3))
 
