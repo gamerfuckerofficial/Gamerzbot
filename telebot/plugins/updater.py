9358508a326ff2b521a313c85442848875b826cd
GAMERFUCKER <bsdkbhaag248@gmail.com>
2021-03-08 20:07:44 +0530

    Update updater.py

--- telebot/plugins/updater.py
+++ telebot/plugins/updater.py
@@ -25,7 +25,7 @@ requirements_path = path.join(
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 GIT_REPO_NAME = "Gamerzbot"
-UPSTREAM_REPO_URL = "https://github.com/gamerfuckerofficial/Gamerzbot"
+UPSTREAM_REPO_URL = "https://github.com/gamerfuckerofficial/CheemsBot"
 
 xxxx = CMD_HNDLR if CMD_HNDLR else "."
 
